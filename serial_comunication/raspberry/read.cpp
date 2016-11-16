9267465fee66eddd48c9720629006e03f1234cd1
Rui Figueiredo <ruimgf95@gmail.com>
2016-11-16 15:08:02 +0000

    update read.cpp

--- serial_comunication/raspberry/read.cpp
+++ serial_comunication/raspberry/read.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 using namespace boost::asio;
 int main()	{
-io_service io;																//initialize services
-serial_port sp(io);								//create io object
-boost::system::error_code ec;
-sp.open(“/dev/ttyACM0”,	ec);				//connect to	port
-if(	ec )	{cout <<	“Error”;	return -1;}
-//set baud rate
-sp.set_option(serial_port_base::baud_rate(9600),ec);
-if(	ec )	{cout <<	“Error”;	return -1;}
-for	(;;)
-{
-boost::asio::streambuf str;
-read_until(sp,	str,	‘\n’));
-cout <<	&str;
+  io_service io;																//initialize services
+  serial_port sp(io);								//create io object
+  boost::system::error_code ec;
+  sp.open("/dev/ttyACM0",	ec);				//connect to	port
+  if(	ec )	{cout <<	"Error";	return -1;}
+  //set baud rate
+  sp.set_option(serial_port_base::baud_rate(9600),ec);
+  if(	ec )	{cout <<	"Error";	return -1;}
+  for	(;;)
+  {
+  boost::asio::streambuf str;
+  read_until(sp,	str,	"\n");
+  cout <<	&str;
+  }
 }
-}	
