a360094265da90fb6bdfd2610d5b38335f07acca
candeiasalexandre <candeiasalexandre@gmail.com>
2016-12-01 16:51:32 +0000

    main_server _serial

--- main_server/main_server.cpp
+++ main_server/main_server.cpp
@@ -14,11 +14,11 @@ raspduino arduinos[NUMBER_ILLUM];
 using namespace std;
 using namespace boost::asio;
 
-void thread_session(int &arduino_number){
+void thread_session(int arduino_number){
 
   while(1){
       arduinos[arduino_number].read_state();
-      arduinos[arduino_number].printvalues;
+      arduinos[arduino_number].printvalues();
   }
 
 
@@ -30,10 +30,7 @@ int main(){
   // replace for a system call ls searching for all ports avaliable
   arduinos[0].init(PORT_ILLUM0);
 
-  for (i=0;i<NUMBER_ILLUM;i++){
-    boost::thread t(boost::bind(thread_session,i));
-  }
+  boost::thread t(boost::bind(thread_session,0));
 
-  t.join;
-
+  t.join();
 }
