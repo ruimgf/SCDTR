fa57334f774f6998e328f4292808cc078ba173ea
candeiasalexandre <candeiasalexandre@gmail.com>
2016-12-01 21:56:09 +0000

    alexandre

--- main_server/main_server.cpp
+++ main_server/main_server.cpp
@@ -17,7 +17,7 @@ using namespace boost::asio;
 void thread_session(int arduino_number){
 
   while(1){
-    
+
       arduinos[arduino_number].read_state();
       arduinos[arduino_number].printvalues();
 
@@ -31,7 +31,7 @@ int main(){
   int i;
   // replace for a system call ls searching for all ports avaliable
   arduinos[0].init(PORT_ILLUM0);
-
+  for
   boost::thread t(boost::bind(thread_session,0));
 
   t.join();
