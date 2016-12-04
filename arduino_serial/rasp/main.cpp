5954996dcf946a366bd59f5eeffc3f6fe9d62ed3
candeiasalexandre <candeiasalexandre@gmail.com>
2016-12-04 17:41:20 +0000

    teste

--- arduino_serial/rasp/main.cpp
+++ arduino_serial/rasp/main.cpp
@@ -1,13 +1,28 @@
-#include "arduino.h"
+#include <SerialPort.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 int main() {
   /* code */
-  arduino ard1{"/dev/ttyACM0"};
-  while (1) {
-    /* code */
-    ard1.retrive_control_loop_data();
+  SerialPort ard("/dev/ttyACM0");
+  ard.Open(SerialPort::BaudRate::BAUD_115200);
+  string data_s;
+
+  cout << "blabla" << endl ;
+  std::this_thread::sleep_for (std::chrono::seconds(5));
+  cout << "blabla" << endl;
+
+  int i = 0;
+  while(i<10){
+
+    data_s = ard.ReadLine();
+    cout << data_s ;
+    i=i+1;
+
   }
-  return 0;
+  ard.Close();
 }
