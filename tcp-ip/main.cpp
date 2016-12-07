8b2067e37d88423968111259c24bee8447b1d4d3
ruimgf <ruimgf95@gmail.com>
2016-12-07 14:37:21 +0000

    tcp tread on demand

--- tcp-ip/main.cpp
+++ tcp-ip/main.cpp
@@ -3,9 +3,11 @@
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
 #include <thread>
-
+#include <boost/array.hpp>
+#include <memory>
 #include "tcp_server.h"
 
+#include <iostream>
 
 
 using namespace boost::asio;
@@ -15,26 +17,60 @@ using ip::tcp;
 tcp_server server1;
 
 
-void thread_session(){
-  while(1){
-    server1.attep_conn();
+void keyboard(){
+  while (1) {
+    string str;
+    std::getline (std::cin,str);
+    if(str == "quit"){
+      exit(0);
+    }
   }
+}
+
+void attep_client(tcp::socket * socket){
+  tcp::socket * socket1 = socket;
+  //std::cout << "in" << '\n';
+  for (;;) {
+    boost::array <char,128>	buf;
+    boost::system::error_code err;
+
+    //std::cout << "go read" << '\n';
+    size_t len;
+    try{
+
+      len =	socket1->read_some(buffer(buf),	err);
+
+    }catch(boost::system::system_error &e){
+
+      boost::system::error_code ec =	e.code();
+      std::cerr << "error in set:"<<	ec.value()	<<	std::endl;
+
+    }
+
+    if (err ==	error::eof)			break;	//	Closed cleanly by peer.
+    else if (err)		std::cout <<	"Unknown Error";
+    //std::cout << "after read" << '\n';
+    string question = buf.data();
+
+    string str;
+
+    std::cout.write(buf.data(),	len);
+
+    str = process_data(question);
+    write(*socket1,	buffer(str));
+  }
+
 
 }
 
 int main(){
   std::cout << "go" << '\n';
-  std::thread t1{thread_session};
-  std::thread t2{thread_session};
-  std::thread t3{thread_session};
-
-
-
+  std::thread t1{keyboard};
+  //t1.detach();
   while (1) {
-    string str;
-    std::getline (std::cin,str);
-    if(str == "quit"){
-      break;
-    }
+    // mutex de guarda
+    tcp::socket * socket = server1.attep_conn();
+    std::thread t1{attep_client,socket};
+    t1.detach();
   }
 }
--- tcp-ip/tcp_server.h
+++ tcp-ip/tcp_server.h
@@ -7,12 +7,14 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
+#include <memory>
 
 #define tcp_ip "127.0.0.1"
 
 using namespace boost::asio;
 using	ip::tcp;
 
+std::string process_data(std::string str );
 
 class tcp_server {
   private:
@@ -20,7 +22,7 @@ class tcp_server {
     boost::asio::ip::tcp::acceptor acceptor{io};
     boost::asio::ip::tcp::endpoint endpoint{boost::asio::ip::tcp::v4(), 17000};
   public:
-    void attep_conn();
+    tcp::socket * attep_conn();
     tcp_server();
 };
 
